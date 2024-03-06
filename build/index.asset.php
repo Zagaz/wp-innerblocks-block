f7472b3fe2a5ccf1ef087ed3ca72b0a93d8d87d6
Zagaz <andre_ranulfo@hotmail.com>
2024-03-06 17:46:56 -0300

    🚿 Clean up!

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fa9b4ddf76cfd8cc7ecb');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '388599bea8139e0d19b9');
