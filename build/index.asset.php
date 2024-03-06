34253ca73b75e5b9eb00d4a81910a60d09c85992
Zagaz <andre_ranulfo@hotmail.com>
2024-03-06 17:31:23 -0300

    It's aliiiive!!!

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f98222d7659977cfd743');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3b8465f5633ab86b9473');
